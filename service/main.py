292b63225394ee9d04feb9bf773f7d58a3c40793
arpitgupta1090 <arpitgupta1090@gmail.com>
2023-04-12 23:23:00 +0530

    API details added on redoc and Docs endpoint

--- service/main.py
+++ service/main.py
@@ -2,7 +2,25 @@ import uvicorn
 from fastapi import FastAPI
 import api
 
-app = FastAPI()
+description = """
+**This API provides a optimised output for the Knapsack problem**
+
+* **Home Page** (You can read the API documentation on the home page "/". 
+                Sample Request and Response are provided on the right side).
+* **Test** (use **/test** endpoint to test the API using sample data or your own data).
+"""
+
+app = FastAPI(
+    title="Knapsack Optimiser",
+    description=description,
+    version="0.1",
+    contact={
+        "name": "Arpit Gupta",
+        "email": "arpitgupta1090@gmail.com",
+    },
+    docs_url="/test",
+    redoc_url="/"
+)
 app.include_router(api.router)
 
 
