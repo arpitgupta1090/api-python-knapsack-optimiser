e34eb14f2c377db8fe787d819ea2ae091ab2c93c
arpitgupta1090 <arpitgupta1090@gmail.com>
2023-04-17 16:01:36 +0530

    [Update] more validation added in request body

--- service/schema/request.py
+++ service/schema/request.py
@@ -1,18 +1,41 @@
-from pydantic import BaseModel
+from enum import Enum
+from pydantic import BaseModel, StrictInt, root_validator
 from typing import List
 from service.openapi.sample_data import request_body
 
 
+class EnumOptimiserName(str, Enum):
+    NAME = "Knapsack Optimiser"
+
+
+class EnumOptimiserType(str, Enum):
+    TYPE_ZERO_ONE = "zero-one"
+    TYPE_PARTIAL = "partial"
+
+
+class EnumOptimiserVersion(str, Enum):
+    VERSION1_0 = "v1.0"
+
+
 class Optimiser(BaseModel):
-    name: str
-    type: str
-    version: str
+    name: EnumOptimiserName
+    type: EnumOptimiserType
+    version: EnumOptimiserVersion
 
 
 class Fields(BaseModel):
-    capacity: int
-    values: List[int]
-    weights: List[int]
+    capacity: StrictInt
+    values: List[StrictInt]
+    weights: List[StrictInt]
+
+    @root_validator(pre=True, allow_reuse=True)
+    def check_names_differs(cls, value: dict):
+        values = value.get('values')
+        weights = value.get('weights')
+
+        if len(values) != len(weights):
+            raise ValueError(f"Number of elements in values and weights must be same.")
+        return value
 
 
 class RequestModel(BaseModel):
